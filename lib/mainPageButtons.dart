5570ee67012e54300f5aadacefdc0c56890ac788
ChengCK18 <chinkheng_98@hotmail.com>
2022-05-31 21:37:03 +0800

    Replaced button with shape and added entry field

--- lib/mainPageButtons.dart
+++ lib/mainPageButtons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MainPageButtons extends StatefulWidget {
   const MainPageButtons({Key? key}) : super(key: key);
@@ -8,81 +9,44 @@ class MainPageButtons extends StatefulWidget {
 }
 
 class _MainPageButtonsState extends State<MainPageButtons> {
+  final controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(children: <Widget>[
-            Container(
-                child: ElevatedButton.icon(
-              icon: Icon(
-                Icons.home,
-                color: Colors.green,
-                size: 30.0,
-              ),
-              label: Text('Elevated Button'),
-              onPressed: () {
-                print('Button Pressed');
-              },
-              style: ElevatedButton.styleFrom(
-                shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(20.0),
-                ),
-              ),
-            )),
-            Container(
-                child: ElevatedButton.icon(
-              icon: Icon(
-                Icons.home,
-                color: Colors.green,
-                size: 30.0,
-              ),
-              label: Text('Elevated Button'),
-              onPressed: () {
-                print('Button Pressed');
-              },
-              style: ElevatedButton.styleFrom(
-                shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(20.0),
-                ),
-              ),
-            )),
-            Container(
-                child: ElevatedButton.icon(
-              icon: Icon(
-                Icons.home,
-                color: Colors.green,
-                size: 30.0,
-              ),
-              label: Text('Elevated Button'),
-              onPressed: () {
-                print('Button Pressed');
-              },
-              style: ElevatedButton.styleFrom(
-                shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(20.0),
-                ),
-              ),
-            )),
-            Container(
-                child: ElevatedButton.icon(
-              icon: Icon(
-                Icons.home,
-                color: Colors.green,
-                size: 30.0,
-              ),
-              label: Text('Elevated Button'),
-              onPressed: () {
-                print('Button Pressed');
-              },
-              style: ElevatedButton.styleFrom(
-                shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(20.0),
-                ),
-              ),
-            ))
-          ])),
+      constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width),
+      child: Center(
+          child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(children: <Widget>[
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 80,
+                        height: 80,
+                        child: const Icon(
+                          Icons.account_balance,
+                          size: 20,
+                        ),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Color(0xFFe0f2f1)),
+                      ),
+                      Container(
+                          constraints: BoxConstraints(maxWidth: 100),
+                          child: TextFormField(
+                              controller: controller,
+                              keyboardType: TextInputType.number,
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.allow(
+                                    RegExp(r'[0-9]')),
+                              ],
+                              decoration: InputDecoration(
+                                  labelText: "amount",
+                                  hintText: "da amount",
+                                  icon: Icon(Icons.phone_iphone))))
+                    ]),
+              ]))),
       margin:
           const EdgeInsets.only(left: 5.0, right: 5.0, top: 5.0, bottom: 5.0),
       decoration: BoxDecoration(
