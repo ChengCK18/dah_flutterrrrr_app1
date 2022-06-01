35ade91bfb58b4569cf7c4027a963058f0c768bd
ChengCK18 <chinkheng_98@hotmail.com>
2022-06-01 23:35:59 +0800

    Layout of bottom panel

--- lib/mainPageButtons.dart
+++ lib/mainPageButtons.dart
@@ -17,35 +17,133 @@ class _MainPageButtonsState extends State<MainPageButtons> {
       child: Center(
           child: SingleChildScrollView(
               scrollDirection: Axis.horizontal,
-              child: Row(children: <Widget>[
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        width: 80,
-                        height: 80,
-                        child: const Icon(
-                          Icons.account_balance,
-                          size: 20,
+              child: Stack(children: <Widget>[
+                Row(children: <Widget>[
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          width: 80,
+                          height: 80,
+                          child: const Icon(
+                            Icons.account_balance,
+                            size: 20,
+                          ),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: Color(0xFFe0f2f1)),
                         ),
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle, color: Color(0xFFe0f2f1)),
-                      ),
-                      Container(
-                          constraints: BoxConstraints(maxWidth: 100),
-                          child: TextFormField(
-                              controller: controller,
-                              keyboardType: TextInputType.number,
-                              inputFormatters: <TextInputFormatter>[
-                                FilteringTextInputFormatter.allow(
-                                    RegExp(r'[0-9]')),
-                              ],
-                              decoration: InputDecoration(
-                                  labelText: "amount",
-                                  hintText: "da amount",
-                                  icon: Icon(Icons.phone_iphone))))
-                    ]),
+                        Container(
+                            margin: EdgeInsets.only(top: 5),
+                            constraints: BoxConstraints(maxWidth: 100),
+                            child: Directionality(
+                                textDirection: TextDirection.ltr,
+                                child: TextField(
+                                  textAlign: TextAlign.center,
+                                  autofocus: true,
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                    FilteringTextInputFormatter.allow(
+                                        RegExp(r'[0-9]')),
+                                  ],
+                                  decoration: new InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: "Amount",
+                                      hintText: "..."),
+                                )))
+                      ]),
+                  SizedBox(
+                    height: 10,
+                    width: 5,
+                  ), //To add spacing between two column
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          width: 80,
+                          height: 80,
+                          child: const Icon(
+                            Icons.account_balance,
+                            size: 20,
+                          ),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: Color(0xFFe0f2f1)),
+                        ),
+                        Container(
+                            margin: EdgeInsets.only(top: 5),
+                            constraints: BoxConstraints(maxWidth: 100),
+                            child: Directionality(
+                                textDirection: TextDirection.ltr,
+                                child: TextField(
+                                  textAlign: TextAlign.center,
+                                  autofocus: true,
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                    FilteringTextInputFormatter.allow(
+                                        RegExp(r'[0-9]')),
+                                  ],
+                                  decoration: new InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: "Amount",
+                                      hintText: "..."),
+                                ))),
+                        Container(
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              // Respond to button press
+                            },
+                            icon: Icon(Icons.add, size: 18),
+                            label: Text("Add"),
+                          ),
+                        )
+                      ]),
+                  SizedBox(
+                    height: 10,
+                    width: 5,
+                  ), //To add spacing between two column
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          width: 80,
+                          height: 80,
+                          child: const Icon(
+                            Icons.account_balance,
+                            size: 20,
+                          ),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: Color(0xFFe0f2f1)),
+                        ),
+                        Container(
+                            margin: EdgeInsets.only(top: 5),
+                            constraints: BoxConstraints(maxWidth: 100),
+                            child: Directionality(
+                                textDirection: TextDirection.ltr,
+                                child: TextField(
+                                  readOnly: true,
+                                  textAlign: TextAlign.center,
+                                  autofocus: true,
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                    FilteringTextInputFormatter.allow(
+                                        RegExp(r'[0-9]')),
+                                  ],
+                                  decoration: new InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: "Total Spending",
+                                      hintText: "55.32"),
+                                )))
+                      ]),
+                ]),
+                Container(
+                    constraints: BoxConstraints(
+                        minWidth: MediaQuery.of(context).size.width - 100),
+                    child: Text(
+                      "Koko desu",
+                      textAlign: TextAlign.left,
+                    ))
               ]))),
       margin:
           const EdgeInsets.only(left: 5.0, right: 5.0, top: 5.0, bottom: 5.0),
